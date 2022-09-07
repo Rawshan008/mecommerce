53e8869d29ba2d7754c797e6c2a8b9c5900b7a9a
Rawshan Ali <pust.love008@gmail.com>
2022-09-07 15:39:46 +0600

    added bootstrap

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-// use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\Admin\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +22,9 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::get('dashboard', [App\Http\Controllers\HomeController::class, 'dashboard']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::prefix('admin')->group(function() {
+    Route::get('dashboard', [DashboardController::class, 'index']);
+});
